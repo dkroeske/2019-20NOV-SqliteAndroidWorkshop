3696c6ffa2f2fbdfe7362e95cd172c5d0de4fe54
Diederich Kroeske <dkroeske@popd.nl>
2019-11-19 11:23:01 +0100

    Workshop START

--- app/src/main/java/com/appsfromholland/sqlitecrud/MainActivity.java
+++ app/src/main/java/com/appsfromholland/sqlitecrud/MainActivity.java
@@ -3,12 +3,53 @@ package com.appsfromholland.sqlitecrud;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnC, btnR, btnU, btnD;
+    TextView dbContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Button CREATE
+        findViewById(R.id.btnC).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        // Button READ
+        findViewById(R.id.bntR).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        // Button UPDATE
+        findViewById(R.id.bntU).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        // Button DELETE
+        findViewById(R.id.btnD).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        dbContent = findViewById(R.id.textView);
+
     }
 }
